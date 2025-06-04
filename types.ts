cb4706ddaf648127d2afb1d290de87272756cd16
Aytunç Demir <112761562+aytuncdemr@users.noreply.github.com>
2025-06-04 18:39:04 +0300

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,23 +1,30 @@
-interface Customer { // customers collection
+interface Customer {
+    // customers collection
+    id: string;
     name: string;
     surname: string;
-    id: string;
     phone: string;
     email: string;
-    password: string;
+    city: string;
+    password: string; // encrypted password
     createdAt: string;
     favoriedBusinesses: string[]; // business ids
-    userType: "customer";
+    accountType: "customer";
 }
 
-interface Business { // businesses collection
+// ----------------------------------------
+interface Business {
+    // businesses collection
     name: string;
     id: string;
     phone: string;
     email: string;
-    password: string;
+    password: string; // encrypted password
     profilePictureUrl: string;
-    category: string;
+    galleryUrls: string[];
+    favoritesCount: number;
+    businessCategory: string;
+    city: string;
     description: string;
     address: string;
     createdAt: string;
@@ -26,7 +33,7 @@ interface Business { // businesses collection
     serviceCategories: ServiceCategory[];
     workers: string[]; // murat...
     averageStar: number;
-    userType: "business";
+    accountType: "business";
 }
 
 interface ServiceCategory {
@@ -37,37 +44,40 @@ interface ServiceCategory {
 interface Service {
     workersName: string[];
     price: number;
+    name: string;
 }
+// ----------------------------------------
 
-
-interface Appointment{ // appointments collection
-
+interface Appointment {
+    // appointments collection
     id: string;
     customerId: string;
     businessId: string;
-    serviceCategory: string; // Saç kesimi...
-    service: string;
+    serviceCategory: string; // Erkek,kadın...
+    serviceName: string; // Saç kesimi, manikür...
+    price: number;
     worker: string; // Murat
     date: string; // 2023-10-01
-    time: string; // 12.00    
-
+    time: string; // 12.00, 12.30...
 }
 
-interface Notification{ // notifications collection
+interface Notification {
+    // notifications collection
     id: string;
     title: string;
     description: string;
     createdAt: string;
     isRead: boolean;
-    to: string;  // customer id
-    from:string; // business id   
+    to: string; // customer id
+    from: string; // business id
 }
 
-interface Comment{ // comments collection
+interface Comment {
+    // comments collection
     id: string;
     customerId: string;
     businessId: string;
-    title:string;
+    title: string;
     worker: string; // Murat
     comment: string;
     star: number; // 1-5
